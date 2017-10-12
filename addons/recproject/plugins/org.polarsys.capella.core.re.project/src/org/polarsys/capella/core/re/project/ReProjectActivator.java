587d234cbef040920c1700f7ed8457c882e13b14
Felix Dorner <felix.dorner@gmail.com>
2017-10-12 13:16:54 +0200

    [1780] REC creation now works for Libraries too
    Change-Id: I9aa1eb0101e78c988194a6fec01f3bd63d93372a
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- addons/recproject/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/ReProjectActivator.java
+++ addons/recproject/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/ReProjectActivator.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *   
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -16,10 +16,11 @@ import org.osgi.framework.BundleContext;
 
 public class ReProjectActivator implements BundleActivator {
 
-  public static final String PLUGIN_ID = "org.polarsys.capella.core.re.project";
+  public static final String PLUGIN_ID = "org.polarsys.capella.core.re.project"; //$NON-NLS-1$
 	private static BundleContext context;
 
-	public static final URI SKELETON_PROJECT_URI = URI.createPlatformPluginURI("/" + PLUGIN_ID + "/skeleton/skeletonProject.melodymodeller", false);
+	public static final URI SKELETON_PROJECT_URI = URI.createPlatformPluginURI("/" + PLUGIN_ID + "/skeleton/skeletonProject.melodymodeller", false); //$NON-NLS-1$ //$NON-NLS-2$
+	public static final URI SKELETON_LIBRARY_URI = URI.createPlatformPluginURI("/" + PLUGIN_ID + "/skeleton/skeletonLibrary.melodymodeller", false); //$NON-NLS-1$ //$NON-NLS-2$
 
 	static BundleContext getContext() {
 		return context;
--- addons/recproject/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/handlers/CreateRECProjectHandler.java
+++ addons/recproject/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/handlers/CreateRECProjectHandler.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *   
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -17,6 +17,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.diffmerge.api.IComparison;
 import org.eclipse.emf.diffmerge.api.Role;
 import org.eclipse.emf.diffmerge.api.diff.IDifference;
@@ -27,6 +28,7 @@ import org.eclipse.emf.diffmerge.impl.scopes.FragmentedModelScope;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -36,6 +38,7 @@ import org.polarsys.capella.common.helpers.EcoreUtil2;
 import org.polarsys.capella.common.re.RePackage;
 import org.polarsys.capella.common.re.RecCatalog;
 import org.polarsys.capella.common.re.ui.handlers.uihead.UIHeadHandler;
+import org.polarsys.capella.core.data.capellamodeller.CapellamodellerPackage;
 import org.polarsys.capella.core.data.ctx.SystemFunction;
 import org.polarsys.capella.core.data.fa.ComponentFunctionalAllocation;
 import org.polarsys.capella.core.re.commands.CreateRecCommand;
@@ -49,35 +52,55 @@ public class CreateRECProjectHandler extends AbstractHandler {
 
     IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
     EObject first = (EObject) selection.getFirstElement();
+
     Resource res = first.eResource();
-
-    ExecutionManager skeletonManager = ExecutionManagerRegistry.getInstance().addNewManager();
-    ResourceSet set = skeletonManager.getEditingDomain().getResourceSet();
-    Resource skeletonResource = set.getResource(ReProjectActivator.SKELETON_PROJECT_URI, true);
-
-    IEditableModelScope targetScope = new FragmentedModelScope(res, true); // For example
-    IEditableModelScope referenceScope = new FragmentedModelScope(skeletonResource, true); // For example
-
-    IComparison comparison = new EComparisonImpl(targetScope, referenceScope);
-    comparison.compute(new SkeletonMatchPolicy(), null, null, new NullProgressMonitor());
-
-    Collection<EObject> elementsForRec = new ArrayList<EObject>();
-    for (IDifference diff : comparison.getRemainingDifferences()) {
-      if (diff instanceof IElementPresence && ((IElementPresence) diff).getPresenceRole() == Role.TARGET) {
-        IElementPresence presence = (IElementPresence) diff;
-        EObject element = presence.getElement();
-        if (accept(element)) {
-          elementsForRec.add(element);
-        }
-      }
+    URI skeletonURI = null;
+    if (EcoreUtil.getRootContainer(first).eClass() == CapellamodellerPackage.Literals.LIBRARY) {
+      skeletonURI = ReProjectActivator.SKELETON_LIBRARY_URI;
+    } else if (EcoreUtil.getRootContainer(first).eClass() == CapellamodellerPackage.Literals.PROJECT) {
+      skeletonURI = ReProjectActivator.SKELETON_PROJECT_URI;
     }
 
-    ExecutionManagerRegistry.getInstance().removeManager(skeletonManager);
+    Collection<EObject> elementsForRec = new ArrayList<EObject>();
 
-    ExecutionManager manager = ExecutionManagerRegistry.getInstance().getExecutionManager(TransactionUtil.getEditingDomain(first));
-    CreateRecCommand command = new CreateRecCommand(elementsForRec, new NullProgressMonitor());
-    command.addParameters(new UIHeadHandler(false));
-    manager.execute(command);
+    if (skeletonURI != null) {
+
+      ExecutionManager skeletonManager = ExecutionManagerRegistry.getInstance().addNewManager();
+
+      try {
+
+        ResourceSet set = skeletonManager.getEditingDomain().getResourceSet();
+        Resource skeletonResource = set.getResource(skeletonURI, true);
+
+        IEditableModelScope targetScope = new FragmentedModelScope(res, true); // For example
+        IEditableModelScope referenceScope = new FragmentedModelScope(skeletonResource, true); // For example
+
+        IComparison comparison = new EComparisonImpl(targetScope, referenceScope);
+        comparison.compute(new SkeletonMatchPolicy(), null, null, new NullProgressMonitor());
+
+        for (IDifference diff : comparison.getRemainingDifferences()) {
+          if (diff instanceof IElementPresence && ((IElementPresence) diff).getPresenceRole() == Role.TARGET) {
+            IElementPresence presence = (IElementPresence) diff;
+            EObject element = presence.getElement();
+            if (accept(element)) {
+              elementsForRec.add(element);
+            }
+          }
+        }
+
+      } finally {
+        ExecutionManagerRegistry.getInstance().removeManager(skeletonManager);
+      }
+
+    }
+
+    if (elementsForRec.size() > 0) {
+
+      ExecutionManager manager = ExecutionManagerRegistry.getInstance().getExecutionManager(TransactionUtil.getEditingDomain(first));
+      CreateRecCommand command = new CreateRecCommand(elementsForRec, new NullProgressMonitor());
+      command.addParameters(new UIHeadHandler(false));
+      manager.execute(command);
+    }
 
     return null;
   }
